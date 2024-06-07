5f5da78729a39a29175d1b29bd73f4149528738a
Tushar Naresh <tushar.naresh@anywhere.co>
2024-06-07 11:32:42 +0530

    mobile view carousel

--- Figma1/index.js
+++ Figma1/index.js
@@ -4,3 +4,22 @@ hamburgerIcon.addEventListener('click',(e)=>{
     e.stopPropagation();
     menuItems.classList.toggle('active');
 })
+
+
+const imgs = document.querySelectorAll('.cards .card');
+
+const slider = document.querySelector('.slider');
+const arrowButtons = document.querySelectorAll('[data-index-change]');
+let currIndex = 0;
+function slide(nextIndex) {
+  if (nextIndex < 0) nextIndex = imgs.length - 1;
+  if (nextIndex >= imgs.length) nextIndex = 0;
+  slider.style.transform = `translateX(-${(nextIndex / imgs.length) * 100}%)`;
+  currIndex = nextIndex;
+}
+arrowButtons.forEach((button) => {
+  button.addEventListener('click', () => {
+    const indexChange = +button.getAttribute('data-index-change');
+    slide(currIndex + indexChange);
+  });
+});
